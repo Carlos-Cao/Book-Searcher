956c79c1db5d10a7e62d218ed0fee13d8cd9f55c
carlos-cao <carlos.cao2@gmail.com>
2020-07-26 15:32:52 +1200

    Removed Unused code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,21 +3,10 @@ import "./App.css";
 import SearchBar from "./Components/SearchBarComponent/SearchBar";
 import MediaGrid from "./Components/MediaGridComponent/MediaGrid";
 import { IUserInput } from "./Common/Interfaces";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
-const theme = createMuiTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 700,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
-    },
-  },
-});
+
 function App() {
   const [UserInput, setUserInput] = useState<IUserInput>({
-    SearchQuery: "space",
+    SearchQuery: "cooking",
   });
   function SetUserInput(a: IUserInput) {
     setUserInput(a);
