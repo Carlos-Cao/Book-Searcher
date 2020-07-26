a595de9ff682ca4e683e5a9a658312fae7dec5b0
carlos-cao <carlos.cao2@gmail.com>
2020-07-26 15:14:29 +1200

    Add Files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import './App.css';
-import SearchBar from './Components/SearchBar/SearchBar';
-
+import React, { useState } from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBarComponent/SearchBar";
+import MediaGrid from "./Components/MediaGridComponent/MediaGrid";
+import { IUserInput } from "./Common/Interfaces";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 700,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 function App() {
+  const [UserInput, setUserInput] = useState<IUserInput>({
+    SearchQuery: "space",
+  });
+  function SetUserInput(a: IUserInput) {
+    setUserInput(a);
+  }
   return (
     <div className="App">
-      <h1> Hello World</h1>
-
-
-
+      <SearchBar SetUserInput={SetUserInput} />
+      <MediaGrid SearchQuery={UserInput.SearchQuery} />
     </div>
   );
 }
---
+++ my-app/src/Components/MediaGridComponent/MediaGrid.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import MediaCard from '../MediaCardComponent/MediaCard';
+import { Grid } from '@material-ui/core';
+import './MediaGrid.css';
+
+interface IState {
+    selfLink: any;
+    volumeInfo: any;
+}
+
+interface IMediaGridProps {
+    SearchQuery: string | null;
+}
+function MediaGrid(props: IMediaGridProps) {
+    const [ItemArray, setItemArray] = useState<IState[]>
+        ([{ selfLink: "", volumeInfo: {} }
+        ]);
+
+    useEffect(() => {
+        fetch("https://www.googleapis.com/books/v1/volumes?q=" + props.SearchQuery)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response.totalItems)
+                setItemArray(response.items)
+            })
+            .catch(() => console.log("it didn't work")
+            );
+
+    }, [props.SearchQuery]);
+    var Cards: JSX.Element[] = [];
+    ItemArray.forEach((el: IState, i: Number) => {
+        if (!el || !el.volumeInfo.imageLinks) {
+            return;
+        }
+
+        Cards.push(
+            <Grid
+                key={"card_" + i}
+                item
+                sm={6}
+                md={4}
+                lg={3}
+                className="MediaGridCard"
+            >
+                <MediaCard
+                    ImageUrl={el.volumeInfo.imageLinks.thumbnail}
+                    title={el.volumeInfo.title}
+                    Description={el.volumeInfo.description}
+                />
+            </Grid>)
+    });
+
+    return (
+        <div>
+            <Grid container spacing={3} className="MediaGridContainer">
+                {Cards}
+            </Grid>
+        </div>
+    )
+}
+
+export default MediaGrid
